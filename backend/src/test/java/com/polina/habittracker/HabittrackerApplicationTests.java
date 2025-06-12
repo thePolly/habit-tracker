4eb8b1f4375b524a6fd5818eb3973919ecd693b2
Polina Katkova <39344446+thePolly@users.noreply.github.com>
2025-06-12 21:58:07 +0200

    init commit

---
+++ backend/src/test/java/com/polina/habittracker/HabittrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polina.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabittrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
